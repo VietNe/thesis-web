b7457f47f1a9fd8a51ccad76e878777fe45ef501
Scraggy Duck <nqviet.dev@gmail.com>
2022-06-19 13:46:42 +0700

    update endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,5 @@
 export const environment = {
   production: false,
   // API_URL: "http://localhost:8000/api/v1",
-  API_URL: "http://35.186.156.123:30401/api/v1",
+  API_URL: "http://ec2-52-90-250-254.compute-1.amazonaws.com:30401/api/v1",
 };
